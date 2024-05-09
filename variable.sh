9f448c9673ab2655ce94b0b1070c260c7245a254
yreva <revanthsaiyarlagadda@gmail.com>
2024-05-09 18:07:50 +0530

    updating thge frontend

--- variable.sh
+++ variable.sh
@@ -2,3 +2,10 @@
 
 ## we hold some data - with a name - it is called variable
 ##  if we can assign a name to a set of data - then it is called as a variable
+
+ ## syntax   =     var-data
+ #        x=10
+ # access:  $var  (or) ${var}
+
+ x=10
+ echo x is $x
